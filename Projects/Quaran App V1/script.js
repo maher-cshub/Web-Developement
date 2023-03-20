173e891ede5e2c601b3166ee41b554f201f274f0
maher-cshub <maherarab.1299@gmail.com>
2023-03-20 23:21:54 +0100

    part1

---
+++ Projects/Quaran App V1/script.js
@@ -0,0 +1,25 @@
+baseURL = "https://mp3quran.net/api/v3"
+const datafetched = []
+const languages = document.getElementById("languages")
+
+let get_main_results = async(baseURL)=>{
+    try {
+        const fetch_response =  await fetch(`${baseURL}/languages`,{method: "GET"})    
+        const data = await fetch_response.json()
+        let html = ""
+        data["language"].forEach(element => {
+            datafetched.push(element)
+            html += `
+                <li>
+                    <button class="language" id="language">${element["language"]}</button>
+                </li>
+            `
+        });
+        languages.innerHTML = html
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+get_main_results(baseURL)
+
